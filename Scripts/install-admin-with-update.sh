15dc4cf3d51cbaf2ae28588775dcfc9842e7c820
Alexander Mantzoukas <ama@trustpilot.com>
2014-07-28 13:43:19 +0200

    Script that works on linux

--- Scripts/install-admin-with-update.sh
+++ Scripts/install-admin-with-update.sh
@@ -4,3 +4,10 @@ echo Downloading executioner...
 
 echo Downloading and installing update...
 ./Services.Package.Download.exe "{'UpdateServerHostname':'update.msd.am','UpdateServerPort': 12345,'PackageFolder':'./admin/executioner/repository/Update/1/','PackageNames':['Services.Update']}"
+
+rm ./admin/executioner/services.packages.database.dbxml
+rm ./admin/executioner/repository/Update/1/services.packages.database.dbxml
+
+chmod +x ./admin/executioner/*
+
+chmod +x ./admin/executioner/repository/Update/1/*
