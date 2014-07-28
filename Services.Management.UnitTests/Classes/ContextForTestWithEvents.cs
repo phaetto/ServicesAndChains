6d07244e687a38a48afabaf30c24963c426241b4
Alexander Mantzoukas <ama@trustpilot.com>
2014-07-28 13:44:30 +0200

    Add context test class with worker event handling

---
+++ Services.Management.UnitTests/Classes/ContextForTestWithEvents.cs
@@ -0,0 +1,21 @@
+﻿namespace Services.Management.UnitTests.Classes
+{
+    using Chains;
+    using Services.Management.Administration.Worker;
+
+    public class ContextForTestWithEvents : Chain<ContextForTestWithEvents>, IWorkerEvents
+    {
+        public const string SuccessfullyStoppedMessage = "Stopped";
+
+        public string contextVariable = null;
+
+        public void OnStart()
+        {
+        }
+
+        public void OnStop()
+        {
+            contextVariable = SuccessfullyStoppedMessage;
+        }
+    }
+}
