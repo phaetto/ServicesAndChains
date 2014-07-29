22c5b65c51007f17e5632987e9df2d336f5cd9cf
Alexander Mantzoukas <ama@trustpilot.com>
2014-07-29 08:34:48 +0200

    New tests to ensure proper cleanup of resources when closing a service

--- Services.Management.UnitTests/AdministrationServerTests.cs
+++ Services.Management.UnitTests/AdministrationServerTests.cs
@@ -3,13 +3,18 @@
     using Chains.Play.Web;
     using Chains.UnitTests.Classes;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Services.Communication;
+    using Services.Communication.Protocol;
+    using Services.Management.Administration.Executioner;
     using Services.Management.Administration.Server;
+    using Services.Management.Administration.Update;
     using Services.Management.Administration.Worker;
+    using Services.Management.UnitTests.Classes;
 
     [TestClass]
     public class AdministrationServerTests
     {
+        private const string ServiceId = "test 1";
+
         [TestMethod]
         public void ServerHost_WhenAWorkUnitContextConnects_ThenThereIsNoError()
         {
@@ -18,12 +23,111 @@
                 using (new WorkUnitContext(
                     new StartWorkerData
                     {
-                        ServiceName = "test 1",
-                        ContextType = typeof(ContextForTest).FullName,
+                        ServiceName = ServiceId,
+                        ContextType = typeof(ContextForTestWithEvents).FullName,
                         AdminHost = "localhost",
                         AdminPort = 12005
                     }).Do(new StartWorkUnit()))
                 {
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ServerHost_WhenAWorkUnitIsStopped_ThenIsStoppedGracefully()
+        {
+            using (new ServerHost("127.0.0.1", 12006).Do(new EnableAdminServer()))
+            {
+                var workerDataWithModules = new StartWorkerData
+                                            {
+                                                AdminHost = "127.0.0.1",
+                                                AdminPort = 12006,
+                                                ContextType = typeof(ContextForTestWithModules).FullName,
+                                                Id = ServiceId,
+                                                ServiceName = ServiceId,
+                                                ReportUpdateIntervalInSeconds = 1,
+                                            };
+
+                using (var executioner = new WorkerExecutioner(ExecutionMode.Worker, workerDataWithModules, processExit: new NoProcessExit()))
+                {
+                    executioner.Execute();
+
+                    using (var clientContextToAdmin = new Client("127.0.0.1", 12006).Do(new OpenConnection()))
+                    {
+                        clientContextToAdmin
+                            .Do(new Send(new StopWorkerProcess(ServiceId)))
+                            .Do(new WaitUntilServiceIsStopped(ServiceId, 100));
+                    }
+
+                    Assert.AreEqual(WorkUnitState.Stopping, executioner.WorkUnitContext.State);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ServerHost_WhenAWorkUnitIsStoppedAndSupportsEvents_ThenIsStoppedGracefullyAndEventsAreCalled()
+        {
+            using (new ServerHost("127.0.0.1", 12007).Do(new EnableAdminServer()))
+            {
+                var workerDataWithModules = new StartWorkerData
+                                            {
+                                                AdminHost = "127.0.0.1",
+                                                AdminPort = 12007,
+                                                ContextType = typeof(ContextForTestWithEvents).FullName,
+                                                Id = ServiceId,
+                                                ServiceName = ServiceId,
+                                                ReportUpdateIntervalInSeconds = 1,
+                                            };
+
+                using (var executioner = new WorkerExecutioner(ExecutionMode.Worker, workerDataWithModules, processExit: new NoProcessExit()))
+                {
+                    executioner.Execute();
+
+                    using (var clientContextToAdmin = new Client("127.0.0.1", 12007).Do(new OpenConnection()))
+                    {
+                        clientContextToAdmin
+                            .Do(new Send(new StopWorkerProcess(ServiceId)))
+                            .Do(new WaitUntilServiceIsStopped(ServiceId, 100));
+                    }
+
+                    var result = executioner.WrappedContext as ContextForTestWithEvents;
+
+                    Assert.AreEqual(WorkUnitState.Stopping, executioner.WorkUnitContext.State);
+                    Assert.AreEqual(ContextForTestWithEvents.SuccessfullyStoppedMessage, result.contextVariable);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ServerHost_WhenAWorkUnitIsStoppedAndSupportsIDisposable_ThenIsStoppedGracefullyAndDisposed()
+        {
+            using (new ServerHost("127.0.0.1", 12008).Do(new EnableAdminServer()))
+            {
+                var workerDataWithModules = new StartWorkerData
+                                            {
+                                                AdminHost = "127.0.0.1",
+                                                AdminPort = 12008,
+                                                ContextType = typeof(DisposableContextForTest).FullName,
+                                                Id = ServiceId,
+                                                ServiceName = ServiceId,
+                                                ReportUpdateIntervalInSeconds = 1,
+                                            };
+
+                using (var executioner = new WorkerExecutioner(ExecutionMode.Worker, workerDataWithModules, processExit: new NoProcessExit()))
+                {
+                    executioner.Execute();
+
+                    using (var clientContextToAdmin = new Client("127.0.0.1", 12008).Do(new OpenConnection()))
+                    {
+                        clientContextToAdmin
+                            .Do(new Send(new StopWorkerProcess(ServiceId)))
+                            .Do(new WaitUntilServiceIsStopped(ServiceId, 100));
+                    }
+
+                    var result = executioner.WrappedContext as DisposableContextForTest;
+
+                    Assert.AreEqual(WorkUnitState.Stopping, executioner.WorkUnitContext.State);
+                    Assert.AreEqual(DisposableContextForTest.SuccessfullyStoppedMessage, result.contextVariable);
                 }
             }
         }
---
+++ Services.Management.UnitTests/Classes/DisposableContextForTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Services.Management.UnitTests.Classes
+{
+    using System;
+    using Chains;
+
+    public class DisposableContextForTest : Chain<DisposableContextForTest>, IDisposable
+    {
+        public const string SuccessfullyStoppedMessage = "Disposed";
+
+        public string contextVariable = null;
+
+        public void Dispose()
+        {
+            contextVariable = SuccessfullyStoppedMessage;
+        }
+    }
+}
