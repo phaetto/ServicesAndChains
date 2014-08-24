b7c8c7441e9d6a8b1e5580dd2a5036ac65f053a1
Alexander Mantzoukas <ama@trustpilot.com>
2014-08-24 23:26:19 +0200

    If the well-known service exists, load up the service starter to get the optional starting options for this server

---
+++ Services.Management/Administration/Server/StartServicesExtensionsWellKnownService.cs
@@ -0,0 +1,37 @@
+﻿namespace Services.Management.Administration.Server
+{
+    using Chains;
+    using Services.Management.Administration.Worker;
+
+    public sealed class StartServicesExtensionsWellKnownService : IChainableAction<AdministrationContext, AdministrationContext>
+    {
+        private const string ContextType = "Services.Extensions.Contexts.ServiceStarter.ServiceStarterContext";
+        private const string Id = "auto-service-starter";
+        private const string ServiceName = "Services.Extensions";
+        private const string DllName = "Services.Extensions.dll";
+
+        public AdministrationContext Act(AdministrationContext context)
+        {
+            var repoServices = context.DoRemotable(new GetAllRepoServices());
+
+            if (repoServices.RepoServices.ContainsKey(ServiceName))
+            {
+                context.Do(
+                    new StartWorkerProcess(
+                        new StartWorkerData
+                        {
+                            AdminHost = context.AdminServer.Parent.Parent.Hostname,
+                            AdminPort = context.AdminServer.Parent.Parent.Port,
+                            ContextType = ContextType,
+                            HostProcessFileName = context.HostProcessName,
+                            Id = Id,
+                            ServiceName = ServiceName,
+                            DllPath = DllName,
+                            ReportUpdateIntervalInSeconds = 30,
+                        }));
+            }
+
+            return context;
+        }
+    }
+}
