9eaa4adde8116c4bfc2735a26e540f4efd4a124e
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2014-12-05 12:20:36 +0100

    Move extensions for async in its own file

---
+++ Chains/ChainExtensionsAsync.cs
@@ -0,0 +1,32 @@
+﻿namespace Chains
+{
+    using System.Threading.Tasks;
+
+    public static class ChainExtensionsAsync
+    {
+        public static Task<ReturnChainType> Do<T, ReturnChainType>(
+            this Task<T> context,
+            IChainableAction<T, Task<ReturnChainType>> action)
+            where T : Chain<T> where ReturnChainType : class
+        {
+            return context.ContinueWith(x => x.Result.Do(action)).Unwrap();
+        }
+
+        public static Task<ReturnChainType> Do<T, ReturnChainType>(
+            this Task<T> context,
+            IChainableAction<T, ReturnChainType> action)
+            where T : Chain<T> where ReturnChainType : class
+        {
+            return context.ContinueWith(x => x.Result.Do(action));
+        }
+
+        public static Task<ReturnChainType> DoAsync<T, ReturnChainType>(
+            this Task<T> context,
+            IChainableAction<T, ReturnChainType> action)
+            where T : Chain<T>
+            where ReturnChainType : class
+        {
+            return context.ContinueWith(x => x.Result.Do(action));
+        }
+    }
+}
