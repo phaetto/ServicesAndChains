2bfa258f0e8b24c739beabd071087aff62a2adaf
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2014-12-08 21:24:36 +0100

    Add TaskEx to gather task-based common methods that are not available in .NET 4.0

---
+++ Chains/TaskEx.cs
@@ -0,0 +1,22 @@
+﻿namespace Chains
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class TaskEx
+    {
+        static public Task<T> FromResult<T>(T result)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.TrySetResult(result);
+            return tcs.Task;
+        }
+
+        static public Task Delay(int milliseconds)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            new Timer(_ => tcs.SetResult(null)).Change(milliseconds, -1);
+            return tcs.Task;
+        }
+    }
+}
