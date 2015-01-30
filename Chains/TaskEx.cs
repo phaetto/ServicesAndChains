89d8be54dafcdd144e8cda4ca29d58a2c17ed199
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2015-01-30 21:04:07 +0100

    Rename variables

--- Chains/TaskEx.cs
+++ Chains/TaskEx.cs
@@ -7,16 +7,16 @@
     {
         static public Task<T> FromResult<T>(T result)
         {
-            var tcs = new TaskCompletionSource<T>();
-            tcs.TrySetResult(result);
-            return tcs.Task;
+            var taskCompletionSource = new TaskCompletionSource<T>();
+            taskCompletionSource.TrySetResult(result);
+            return taskCompletionSource.Task;
         }
 
         static public Task Delay(int milliseconds)
         {
-            var tcs = new TaskCompletionSource<object>();
-            new Timer(_ => tcs.SetResult(null)).Change(milliseconds, -1);
-            return tcs.Task;
+            var taskCompletionSource = new TaskCompletionSource<object>();
+            new Timer(_ => taskCompletionSource.SetResult(null)).Change(milliseconds, -1);
+            return taskCompletionSource.Task;
         }
     }
 }
