954b48280faa8f825a28f56d042e2de599ecc49d
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2015-10-30 13:04:11 +0100

    Changed to property expression. Yaaaay!

--- Chains/AbstractChain.cs
+++ Chains/AbstractChain.cs
@@ -4,13 +4,7 @@
 
     public abstract class AbstractChain
     {
-        public static bool IsMono
-        {
-            get
-            {
-                return Type.GetType("Mono.Runtime") != null;
-            }
-        }
+        public static bool IsMono => Type.GetType("Mono.Runtime") != null;
 
         protected Action<object> OnBeforeExecuteAction;
         protected Action<object> OnAfterExecuteAction;
