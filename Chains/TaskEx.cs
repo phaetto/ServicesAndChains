a572e68337ae3e0f9afbf5f0e4674b3d7e1bfeb1
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2016-02-07 20:16:21 +0100

    Remove the custom task delay methods

--- Chains/TaskEx.cs
+++ Chains/TaskEx.cs
@@ -18,25 +18,5 @@
             new Timer(_ => taskCompletionSource.SetResult(null)).Change(milliseconds, -1);
             return taskCompletionSource.Task;
         }
-
-        public static Task DelayWithCarbageCollection(int milliseconds)
-        {
-            var taskCompletionSource = new DelayTaskCompletionSource();
-
-            taskCompletionSource.Timer = new Timer(
-                DelayTimerCallback,
-                taskCompletionSource,
-                milliseconds,
-                Timeout.Infinite);
-
-            return taskCompletionSource.Task;
-        }
-
-        private static void DelayTimerCallback(object state)
-        {
-            var taskCompletionSource = (DelayTaskCompletionSource)state;
-            taskCompletionSource.SetResult(true);
-            taskCompletionSource.Dispose();
-        }
     }
 }
